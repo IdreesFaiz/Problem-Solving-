885452fd9fe9d06075506fd32417fef127c9d3a2
M IDREES FAIZ <idreesfaiz63@gmail.com>
2024-08-16 23:37:32 +0500

    Update Easy(1 to 10).js

--- Easy(1 to 10).js
+++ Easy(1 to 10).js
@@ -2,6 +2,7 @@
 //**************1 to 10 *********************
 //******************************************
 
+
 //Question # 1 => Return the Sum of Two Numbers
 var num1 = -3,num2 = 2;
 function sum(num1, num2) {
@@ -9,3 +10,13 @@ function sum(num1, num2) {
   return result;
 }
 console.log(sum(num1, num2));
+
+
+
+//Question # 2 => Convert Minutes into Seconds
+function minutesToSeconds(minutes) {
+  return minutes * 60;
+}
+const minutes = 60;
+const seconds = minutesToSeconds(minutes);
+console.log(`${minutes} minutes is equal to ${seconds} seconds.`);
