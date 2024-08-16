1f5c301b0968ac57f13c55bb5f51c690bea0f17b
M IDREES FAIZ <idreesfaiz63@gmail.com>
2024-08-16 23:28:46 +0500

    Create Easy(1 to 10).js

---
+++ Easy(1 to 10).js
@@ -0,0 +1,11 @@
+//******************************************
+//**************1 to 10 *********************
+//******************************************
+
+//Question # 1 => Return the Sum of Two Numbers
+var num1 = -3,num2 = 2;
+function sum(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
+console.log(sum(num1, num2));
