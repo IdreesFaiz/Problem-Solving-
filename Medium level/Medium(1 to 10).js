0d81647ef65e0222a251de2fa65b5ebfb4101edd
M IDREES FAIZ <idreesfaiz63@gmail.com>
2024-08-25 20:32:12 +0500

    Update Medium(1 to 10).js

--- Medium level/Medium(1 to 10).js
+++ Medium level/Medium(1 to 10).js
@@ -17,3 +17,24 @@ function Find(arr) {
 var arr = [1, 2, 3, [4, 5, [6, [7]]]];
 Find(arr);
 console.log(temp);
+
+
+
+//Question # 2 =>  Two Sum
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+function sum(numb , target){
+  for (let i = 0; i <  numb.length; i++) {
+    console.log(numb[i]);
+    for(let j = 1 ; j< numb.length ; j++){
+      console.log(numb[j]);
+      if((numb[i] + numb[j]) == target){
+        return [i ,j]
+      }
+      else{
+        console.log("no match the pair");
+      }
+    }
+  }
+}
+var numb = [2, 7, 11, 15] , target = 9; 
+console.log(sum(numb, target));
