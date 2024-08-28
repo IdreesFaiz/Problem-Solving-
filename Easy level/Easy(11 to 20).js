1d5e5bf825c6d847188de455a9d219fb389a1439
M IDREES FAIZ <idreesfaiz63@gmail.com>
2024-08-28 22:16:39 +0500

    Update Easy(11 to 20).js

--- Easy level/Easy(11 to 20).js
+++ Easy level/Easy(11 to 20).js
@@ -11,7 +11,19 @@ cal(5, 2, 8);
 
 
 //Question # 12 => Plus One
-// new 
 var increment = [4,3,2,1]
 var work = (+increment.join("") + 1).toString().split("");
 console.log(work);
+
+
+
+//Question # 13 => Swapping a number 
+var num1 = 20 , num2 = 30;
+console.log(num1 , num2);
+num1 = num2 + num1
+num2 = num1 - num2
+num1 =num1 -num2
+console.log(num1 , num2);
+
+
+
